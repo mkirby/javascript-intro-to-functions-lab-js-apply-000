d4aa5b01621e2228cd3eb1c9f8e345ae528a1329
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-06-26 16:39:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ function logShout(string) {
   console.log(string.toUpperCase());
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  // three separate outputs depending on case
+  let upperCase = string.toUpperCase;
+  let lowerCase = string.toUpperCase;
+  
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  }
+  if (string === upperCase) {
+    return 'YES INDEED!';
+  }
+  
+  if (string === lowerCase) {
+    return 'I can\'t hear you!' 
+  }
+}
+
